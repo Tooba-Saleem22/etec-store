eae26a856d8559d19534ff2525491698d3ce69fc
Tooba-Saleem22 <toobasaleem190@gmail.com>
2026-03-31 15:14:49 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import ScrollToTop from "./components/ScrollToTop";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import AllProducts from "./pages/AllProducts";
@@ -10,10 +12,7 @@ import About from "./pages/About";
 import FAQ from "./pages/FAQ";
 import Contact from "./pages/Contact";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-import Dashboard from "./dashboard/Dashboard";
+// import DashboardLayout from "./dashboard/DashboardLayout";
 
 const App = () => {
   return (
@@ -21,15 +20,21 @@ const App = () => {
       <ScrollToTop />
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<AllProducts />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/dashboard/*" element={<Dashboard />} />
-      </Routes>
+      {/* Content starts below navbar */}
+      <div className="pt-20 md:pt-2">
+        <Routes>
+          {/* Public site pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {/* Admin dashboard routes
+          <Route path="/dashboard/*" element={<DashboardLayout />} /> */}
+        </Routes>
+      </div>
 
       <Footer />
     </Router>
